8b4a49c8b77412c4a13110878c34681a77ac0fd1
yiqiedeweiyi <766118613@qq.com>
2016-10-27 10:46:34 +0800

    Init my first app

--- myApp.py
+++ myApp.py
@@ -32,7 +32,7 @@ def getdata():
         db=_mysql.connect(host=sae.const.MYSQL_HOST,port=int(sae.const.MYSQL_PORT),user=sae.const.MYSQL_USER,passwd=sae.const.MYSQL_PASS,db=sae.const.MYSQL_DB)
         db.query("""SELECT * FROM mysql0571""")
         r=db.store_result()
-        myRow=r.fetchall()
+        myRow=r.affected_rows()
         jsonData = {'data': [myRow]}
         return jsonify(jsonData)
 
