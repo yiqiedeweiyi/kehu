819d0a186414e1ab9cf3d98cfc5d76400a2ff2cc
yiqiedeweiyi <766118613@qq.com>
2016-10-27 15:24:01 +0800

    Init my first app

--- myApp.py
+++ myApp.py
@@ -21,7 +21,8 @@ def getdata():
         values=request.values['dataId']
         jsonData={'dataId':values}
         db=MySQLdb.connect(host=sae.const.MYSQL_HOST,port=int(sae.const.MYSQL_PORT),user=sae.const.MYSQL_USER,passwd=sae.const.MYSQL_PASS,db=sae.const.MYSQL_DB,charset='utf8')
-        db.query("""SELECT * FROM mysql0571 WHERE AK编号='"""+values +"' or G编号='"+values+"' or D编号='"+values+"'")
+        # db.query("""SELECT * FROM mysql0571 WHERE AK编号='"""+values +"' or G编号='"+values+"' or D编号='"+values+"'")
+        db.query("""SELECT * FROM mysql0571 """)
         r=db.store_result()
         myRow=r.fetch_row(r.num_rows())
         jsonData = {'data': [myRow]}
