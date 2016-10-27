96c24a4316edd4350a701076fee96a5fbba50374
yiqiedeweiyi <766118613@qq.com>
2016-10-27 10:34:51 +0800

    Init my first app

--- myApp.py
+++ myApp.py
@@ -32,75 +32,9 @@ def getdata():
         db=_mysql.connect(host=sae.const.MYSQL_HOST,port=int(sae.const.MYSQL_PORT),user=sae.const.MYSQL_USER,passwd=sae.const.MYSQL_PASS,db=sae.const.MYSQL_DB)
         db.query("""SELECT * FROM mysql0571""")
         r=db.store_result()
-        myRow=r.fetch_row()
+        myRow=r.mysql_num_rows()
         jsonData = {'data': [myRow]}
         return jsonify(jsonData)
 
-@app.route('/setdata', methods=['POST', 'GET'])
-def setdata():
-    if request.method == 'POST':
-        values=request.values['data']
-        print(values)
-        print(json.loads(values))
-        jsonList=json.loads(values)['data']
-        myJsonList=[]
-        for i in range(len(jsonList)):
-            orderNo=jsonList[i]['orderNo']
-            title=jsonList[i]['title']
-            contactName=jsonList[i]['contactName']
-            fromDate=jsonList[i]['fromDate']
-            toDate=jsonList[i]['toDate']
-            roomName=jsonList[i]['roomName']
-            roomNum=jsonList[i]['roomNum']
-            isVisit=jsonList[i]['isVisit']
-            isProcess=jsonList[i]['isProcess']
-            myJsonList.append({'orderNo':orderNo,'title':title,'contactName':contactName,'fromDate':fromDate,'toDate':toDate,
-                               'roomName':roomName,'roomNum':roomNum,'isVisit':isVisit,'isProcess':isProcess})
-        jsonData={'data':myJsonList}
-        if isLocal:
-            file=open('data.txt','w')
-            file.write(json.dumps(jsonData))
-            file.close()
-        else:
-            bucket = Bucket('filebucket')
-            bucket.put_object('data.txt',str(json.dumps(jsonData)))
-            pass
-        return jsonify(jsonData)
-    else:
-        jsonData = {'data': []}
-        return jsonify(jsonData)
-
-@app.route('/updateData', methods=['POST', 'GET'])
-def updateData():
-    if request.method == 'POST':
-        response={'response':'修改数据不成功！'}
-        orderId=request.values['orderId']
-        isVisit=request.values['isVisit']
-        orderId=orderId[8:]
-        print(orderId)
-        jsonData=set()
-        if isLocal:
-            file=open('data.txt','r')
-            jsonData=json.loads(file.readline())
-            file.close()
-        else:
-            bucket = Bucket('filebucket')
-            jsonData=json.loads(bucket.get_object_contents('data.txt'))
-        for i in range(len(jsonData['data'])):
-            if(int(orderId)==jsonData['data'][i]['orderNo']):
-                jsonData['data'][i]['isVisit']=isVisit
-                jsonData['data'][i]['isProcess']='1'
-                response={'response':'修改数据成功！'}
-        if isLocal:
-            file=open('data.txt','w')
-            file.write(json.dumps(jsonData))
-            file.close()
-        else:
-            bucket = Bucket('filebucket')
-            bucket.put_object('data.txt',str(json.dumps(jsonData)))
-        return jsonify(response)
-    else:
-        jsonData = {'data': []}
-        return jsonify(jsonData)
 if __name__ == '__main__':
     app.run()
